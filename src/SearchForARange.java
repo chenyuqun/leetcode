54aa417aa6861c987980734e3ebb3afb6eef143b
chenyuqun <cyq910506@gmail.com>
2017-05-08 14:55:58 +0800

    多考虑边界情况

--- src/SearchForARange.java
+++ src/SearchForARange.java
@@ -8,48 +8,50 @@
  */
 public class SearchForARange {
     public static int[] searchRange(int[] nums, int target) {
-        int i=0;
-        int j=nums.length-1;
         int[] result=new int[2];
-        if(nums.length==0||nums[i]>target||nums[j]<target){
-            result[0]=-1;
-            result[1]=-1;
-            return result;
-        }
-        while (j!=0){
-            if(nums[j]==target) {
-                break;
-            }else if(nums[j]<target){
-                j=(nums.length-1+j)/2;
-            }else{
-                j=j/2;
-            }
-        }
-        int k=j;int m=j;
-        while(nums[k]==nums[j]){
-            if(k==0){
-                result[0]=0;
-                break;
-            }else{
-                k--;
-            }
-        }
-        result[0]=k;
-        while(nums[m]==nums[j]){
-            if(m==nums.length-1){
-                result[1]=nums.length-1;
-                break;
-            }else{
-                m++;
-            }
-        }
-        result[1]=m;
+        result[0]=findFirst(nums,target);
+        result[1]=findLast(nums,target);
         return result;
     }
 
+    private static int findFirst(int[] nums, int target){
+        int id=-1;
+        int i=0;
+        int j=nums.length-1;
+        while(i<=j){
+            int mid=(i+j)>>1;
+            if(nums[mid]<target){
+                i=mid+1;
+            }else{
+                j=mid-1;
+            }
+            if(nums[mid]==target)
+                id=mid;
+        }
+        return id;
+    }
+
+    private static int findLast(int[] nums, int target){
+        int id=-1;
+        int i=0;
+        int j=nums.length-1;
+        while(i<=j){
+            int mid=(i+j)>>1;
+            if(nums[mid]<=target){
+                i=mid+1;
+            }else{
+                j=mid-1;
+            }
+            if(nums[mid]==target)
+                id=mid;
+        }
+        return id;
+    }
+
+
     public static void main(String[] args){
-        int[] nums={1,1};
-        int target=1;
+        int[] nums={0,1,2,3,4,4,4};
+        int target=2;
         System.out.println(searchRange(nums,target));
 
     }
