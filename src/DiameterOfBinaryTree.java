d708db5820ecaed4094f06dffb7c049d1d381b10
chenyuqun <cyq910506@gmail.com>
2017-06-02 17:46:01 +0800

    二叉树相关,注意周长这题不一定是以根节点为中心

---
+++ src/DiameterOfBinaryTree.java
@@ -0,0 +1,42 @@
+/**
+ * No.543
+ * Given a binary tree, you need to compute the length of the diameter of the tree. The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+ *
+ * Example:
+ * Given a binary tree
+ * 1
+ * / \
+ * 2   3
+ * / \
+ * 4   5
+ * Return 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
+ *
+ * Note: The length of path between two nodes is represented by the number of edges between them.
+ */
+public class DiameterOfBinaryTree {
+    int result=0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        if(root==null) return 0;
+        result=Math.max(diameterOfNode(root),Math.max(diameterOfBinaryTree(root.left),
+                diameterOfBinaryTree(root.right)));
+        return result;
+    }
+
+    private int diameterOfNode(TreeNode root){
+        if(root==null) return 0;
+        return depth(root.left)+depth(root.right);
+    }
+
+    private int depth(TreeNode root){
+        if(root==null) return 0;
+        if(root.left==null&&root.right==null) return 1;
+        return 1+Math.max(depth(root.left),depth(root.right));
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+}
