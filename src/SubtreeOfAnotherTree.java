4ebbbb49b09f1c7bf910ce9759bda0cfb786f50e
chenyuqun <cyq910506@gmail.com>
2017-06-05 10:08:11 +0800

    二叉树递归和堆栈两种解题思路

--- src/SubtreeOfAnotherTree.java
+++ src/SubtreeOfAnotherTree.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 /**
  * No.572
  * Given two non-empty binary trees s and t, check whether tree t has exactly the same structure and node values with a subtree of s. A subtree of s is a tree consists of a node in s and all of this node's descendants. The tree s could also be considered as a subtree of itself.
@@ -32,10 +34,46 @@
  * Return false.
  */
 public class SubtreeOfAnotherTree {
+    //递归
     public boolean isSubtree(TreeNode s, TreeNode t) {
-        return true;
+        if (s == null) return false;
+        if (isSame(s, t)) return true;
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
     }
 
+    private boolean isSame(TreeNode s, TreeNode t) {
+        if (s == null && t == null) return true;
+        if (s == null || t == null) return false;
+
+        if (s.val != t.val) return false;
+
+        return isSame(s.left, t.left) && isSame(s.right, t.right);
+    }
+
+
+    public boolean isSubtree2(TreeNode s, TreeNode t) {
+        String spreorder = generatepreorderString(s);
+        String tpreorder = generatepreorderString(t);
+
+        return spreorder.contains(tpreorder) ;
+    }
+    public String generatepreorderString(TreeNode s){
+        StringBuilder sb = new StringBuilder();
+        Stack<TreeNode> stacktree = new Stack();
+        stacktree.push(s);
+        while(!stacktree.isEmpty()){
+            TreeNode popelem = stacktree.pop();
+            if(popelem==null)
+                sb.append(",#"); // Appending # inorder to handle same values but not subtree cases
+            else
+                sb.append(","+popelem.val);
+            if(popelem!=null){
+                stacktree.push(popelem.right);
+                stacktree.push(popelem.left);
+            }
+        }
+        return sb.toString();
+    }
     public class TreeNode {
         int val;
         TreeNode left;
